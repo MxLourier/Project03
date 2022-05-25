b54ec99b814e6679a96f018e9f1d121e255bd27c
Maya Lourier <maya.v.lourie@gmail.com>
2022-05-25 15:20:23 +0300

    Solved issue with JSON processing, added 'shortArticle' template into 'mainPage' template

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const https = require("https")
-app=express();
+app = express();
 
 app.set('view engine', 'ejs');
 
@@ -8,32 +8,52 @@ app.set('view engine', 'ejs');
 const url = "https://api.wisetrout.net/jsonapi/node/page";
 var articles = [];
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
 
-app.get("/", function(req, res){
-  https.get(url, function(response){
-    response.on("data", function(data){
-      const APIdata = JSON.parse(data) //Sometimes works and sometimes doesn't, why?
-      console.log(APIdata);
-      for(let i = 0; i<APIdata.data.length; i++){
-        const article = {
-          name: APIdata.data[i].attributes.title,
-          id: APIdata.data[i].id,
-          content: APIdata.data[i].attributes.body.value};
-        articles.push(article);
-      }
-      res.render('mainPage', {articleList: articles}); //can this be done using promises?
-      //https.get is async, res.send should be executed after https.get has finished
+app.get("/", function(req, res) {
+  https.get(url, function(response) {
+
+    var APIdata = {};
+    var rawData = "";
+    response.on('data', (chunk) => {
+      rawData += chunk;
     });
-  })
+    // HTTP response object can receive multiple 'data' events,
+    //each time getting only chunk of the full data
+    // used example from https://nodejs.org/api/http.html ("http.get")
+    //to accumulate data events
+    response.on('end', () => {
+      try {
+        APIdata = JSON.parse(rawData);
+        //console.log(APIdata);
+
+        for (let i = 0; i < APIdata.data.length; i++) {
+          const article = {
+            name: APIdata.data[i].attributes.title,
+            id: APIdata.data[i].id,
+            content: APIdata.data[i].attributes.body.value
+          };
+          articles.push(article);
+        }
+        res.render('mainPage', {
+          articleList: articles
+        });
+
+      } catch (e) {
+        console.error(e.message);
+      }
+    })
+  });
 })
 
-app.get("/page/:id", function(req,res){
+app.get("/page/:id", function(req, res) {
+  //show page of the article that has an id equal to <:id>
   const requestedArticleId = req.params.id;
   chosenArticle = articles.find(element => element.id === requestedArticleId);
-  console.log(chosenArticle.name);
-  console.log(chosenArticle.content);
-  res.render("longArticle", {articleName:chosenArticle.name, articleContent: chosenArticle.content})
+  res.render("longArticle", {
+    articleName: chosenArticle.name,
+    articleContent: chosenArticle.content
+  })
 })
