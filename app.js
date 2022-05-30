d4ba2805ae74b41e866d183546390b9ba9b9b4a5
Maya Lourier <maya.v.lourie@gmail.com>
2022-05-30 17:03:53 +0300

    Added 404 page for get('/page/<id>') in case of invalid <id>

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.listen(3000, function() {
 });
 
 app.get("/", function(req, res) {
+  articles = [];
+  //without this line every time the main page is refreshed
+  //articles keep being added to article list again
   https.get(url, function(response) {
 
     var APIdata = {};
@@ -40,9 +43,7 @@ app.get("/", function(req, res) {
         res.render('mainPage', {
           articleList: articles
         });
-        articles = [];
-        //without this line every time the main page is refreshed
-        //articles keep being added to article list again
+
 
       } catch (e) {
         console.error(e.message);
@@ -54,35 +55,47 @@ app.get("/", function(req, res) {
 app.get("/page/:id", function(req, res) {
       //show page of the article that has an id equal to <:id>
       const requestedArticleId = req.params.id;
-      https.get(url + "/" + requestedArticleId, function(response) {
-      //getting article content and article name again (not using old values
-      // from get("/")). This is done in order to
-      //a) refresh article content (in case there was a change after user
-      // requested main page) - caching
-      //b)get full article content - in case of a long article content requesting
-      // article content by using simply https.get(url) will only give us a summary
-      //of body.value
-
-          var APIdata = {};
-          var rawData = "";
-          response.on('data', (chunk) => {
-            rawData += chunk;
+      const chosenArticle = articles.find(element => element.id === requestedArticleId);
+      if (!chosenArticle){
+        //if no article with the requested id exists
+        res.render('longArticle', {
+            articleName: "404",
+            articleContent: "Page not found"
           });
-          // HTTP response object can receive multiple 'data' events,
-          //each time getting only chunk of the full data
-          // used example from https://nodejs.org/api/http.html ("http.get")
-          //to accumulate data events
-          response.on('end', () => {
-              try {
-                APIdata = JSON.parse(rawData);
-                res.render('longArticle', {
-                    articleName: APIdata.data.attributes.title,
-                    articleContent: APIdata.data.attributes.body.value
-                  });
-                }
-                catch (e) {
-                  console.error(e.message);
-                }
-              })
-          })
+      }
+
+      else {
+        https.get(url + "/" + requestedArticleId, function(response) {
+        //getting article content and article name again (not using old values
+        // from get("/")). This is done in order to
+        //a) refresh article content (in case there was a change after user
+        // requested main page) - caching
+        //b)get full article content - in case of a long article content requesting
+        // article content by using simply https.get(url) will only give us a summary
+        //of body.value
+
+            var APIdata = {};
+            var rawData = "";
+            response.on('data', (chunk) => {
+              rawData += chunk;
+            });
+            // HTTP response object can receive multiple 'data' events,
+            //each time getting only chunk of the full data
+            // used example from https://nodejs.org/api/http.html ("http.get")
+            //to accumulate data events
+            response.on('end', () => {
+                try {
+                  APIdata = JSON.parse(rawData);
+                  res.render('longArticle', {
+                      articleName: APIdata.data.attributes.title,
+                      articleContent: APIdata.data.attributes.body.value
+                    });
+                  }
+                  catch (e) {
+                    console.error(e.message);
+                  }
+                })
+            })
+      }
+
       })
